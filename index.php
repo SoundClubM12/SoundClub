175679ea26166da89b2dd8e18a3d7faa2dd442d8
Visible <Visible>
2016-12-14 11:24:42 +0100

    no message

--- index.php
+++ index.php
@@ -7,15 +7,12 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>SoundClub - Home</title>
-        <link 
+        <title>SoundClub</title>
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.css" />
     </head>
     <body>
-        <div class="topbar">
-            asdfs
-        </div>
+        <h1>SoundClub</h1>
         <?php
-            
         ?>
     </body>
 </html>
