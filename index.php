59e3a80c29937abb3d2825f71734a13f7fbea93c
Dani <soundclubM12@gmail.com>
2017-01-10 13:07:09 +0100

    index

--- index.php
+++ index.php
@@ -11,8 +11,29 @@ and open the template in the editor.
         <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.css" />
     </head>
     <body>
+      <div id="contenedor">
+        <div id="cabecera">
+            <a href class="izquierda">Soundclub</a>
+            <a href class="login">Login</a>
+            <a href class="derecha">Terms</a>
+            <a href class="derecha">Contact</a>
+            <a href class="derecha">Sign Up</a>
+        </div>
         <h1>SoundClub</h1>
+        <h2>Listen, Create, Share</h2>
+        <div id="registro">
+          <a>Already have an account?</a>
+          <a href id="login">Login</a>
+          <br /><br /><hr />
+          <form method="post" enctype="multipart/form-data" action="index.php">
+            <input type="text" class="field"/><br />
+            <input type="text" class="field"/><br />
+            <input type="password" class="field"/><br />
+            <input type="password" class="field"/><br />
+          </form>
+        </div>
         <?php
         ?>
+      </div>
     </body>
 </html>
