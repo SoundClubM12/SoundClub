0fe0f26b30adef5ebb2137a3e4323247ae347cd2
Dani <soundclubM12@gmail.com>
2017-01-18 12:27:50 +0100

    Hostinger

--- Connection/Class/connection.php
+++ Connection/Class/connection.php
@@ -1,8 +1,8 @@
 <?php
 class Conectar{
   public static function connection(){
-    $connection = mysql_connect("localhost", "root","");
-    mysql_select_db("SoundClub");
+    $connection = mysql_connect("mysql.hostinger.es", "u590849717_sound","patatafrita");
+    mysql_select_db("u590849717_sound");
     return $connection;
   }
 }
